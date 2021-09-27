1b370b3c2e64957641d035e9cd2c4c5ed745e21d
2sagarraut2 <https://github.com/2sagarraut2/helloworld.git>
2021-09-27 20:44:14 +0530

    first commit

---
+++ src/component/Layout.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import { Drawer, Typography, List, ListItem, ListItemIcon, ListItemText, AppBar, Toolbar } from '@material-ui/core';
+import { AddCircleOutlined, SubjectOutlined } from '@material-ui/icons';
+import { useHistory, useLocation } from 'react-router';
+import { format } from 'date-fns'
+import Avatar from '@material-ui/core/Avatar';
+
+const drawerWidth = 240
+
+const useStyles = makeStyles((theme) => {
+    return {
+        page: {
+            background: '#f9f9f9',
+            width: "100%",
+            padding: theme.spacing(3)
+        },
+        drawer: {
+            width: drawerWidth,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        root: {
+            display: 'flex',
+        },
+        active: {
+            background: '#f4f4f4 !important'
+        },
+        title: {
+            padding: theme.spacing(2)
+        },
+        appbar: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+        },
+        date: {
+            flexGrow: 1
+          },
+        toolbar: theme.mixins.toolbar,
+        avatar: {
+            marginLeft: theme.spacing(2)
+        }
+    }
+})
+
+const Layout = ({ children }) => {
+
+    const classes = useStyles();
+    const history = useHistory();
+    const location = useLocation();
+
+    const menuItems = [
+        {
+            text: 'My Notes',
+            icon: <SubjectOutlined color="secondary" />,
+            path: "/"
+        },
+        {
+            text: 'Create Notes',
+            icon: <AddCircleOutlined color="secondary" />,
+            path: "/create"
+        },
+    ]
+
+    return (
+        <div className={classes.root}>
+            {/* app bar */}
+            <AppBar
+                className={classes.appbar}
+                elevation={0}
+            >
+                <Toolbar>
+                    <Typography className={classes.date}>
+                        Today is the { format(new Date(), 'do MMMM Y') }
+                    </Typography>
+                    <Typography>
+                        Mario
+                    </Typography>
+                    <Avatar src="/mario.png" className={classes.avatar} />
+                </Toolbar>
+            </AppBar>
+
+            {/* side draw */}
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                anchor="left"
+                classes={{ paper: classes.drawerPaper }}
+            >
+                <div>
+                    <Typography variant="h5" className={classes.title}>
+                        My Notes
+                    </Typography>
+                </div>
+
+                {/* list / links */}
+                <List>
+                    {menuItems.map(item => (
+                        <ListItem
+                            button
+                            key={item.text}
+                            onClick={() => history.push(item.path)}
+                            className={location.pathname === item.path ? classes.active : null}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+
+            <div className={classes.page}>
+                <div className={classes.toolbar}></div>
+                {children}
+            </div>
+        </div>
+    )
+};
+
+export default Layout;
---
+++ src/pages/Create.js
@@ -0,0 +1,118 @@
+import { Button, Container, FormControlLabel, Radio, RadioGroup, TextField, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+// import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRightIcon'
+import { makeStyles } from '@material-ui/core';
+import { FormLabel, FormControl } from '@material-ui/core';
+import { useHistory } from 'react-router';
+
+const useStyles = makeStyles({
+    field: {
+      marginBottom: '20px !important',
+      display: 'block !important'
+    },
+});
+
+const Create = () => {
+
+  const classes = useStyles();
+  const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+  const [category, setCategory] = useState("Money")
+
+  const handleOnChange = (e) => {
+    
+  }
+
+  const onSubmit = () => {
+    console.log(title, details);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if(title === "") {
+      setTitleError(true);
+    }
+
+    if(details === "") {
+      setDetailsError(true);
+    }
+
+    if(title && details) {
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({ title, details, category })
+      }).then(() => history.push('/'));
+    }
+  }
+
+  return (
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a new note
+      </Typography>
+
+      <form noValidate autoComplete="off">
+        <TextField
+          name="title"
+          value={title}
+          className={classes.field}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          error={titleError}
+        />
+        <TextField
+          name="details"
+          value={details}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          onChange={(e) => setDetails(e.target.value)}
+          error={detailsError}
+        />
+      </form>
+
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+            <FormControlLabel value="Money" control={<Radio />} label="Money"></FormControlLabel>
+            <FormControlLabel value="Todos" control={<Radio />} label="Todo"></FormControlLabel>
+            <FormControlLabel value="Reminders" control={<Radio />} label="Reminder"></FormControlLabel>
+            <FormControlLabel value="Work" control={<Radio />} label="Work"></FormControlLabel>
+          </RadioGroup>
+        </FormControl>
+
+      <Button
+        type="Submit"
+        color="secondary"
+        variant="contained"
+        // endIcon={<KeyboardArrowRightIcon />}
+        onClick={(e) => handleSubmit(e)}
+      >
+        SUBMIT
+      </Button>
+    </Container>
+  )
+}
+
+export default Create;
